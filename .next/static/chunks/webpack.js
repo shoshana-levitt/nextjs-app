ed27c7c331e472e1eb93256913e13b7cc5eb455a
shoshana.levitt@gmail.com <shoshana.levitt@gmail.com>
2023-11-06 16:55:16 -0500

    fix typo on index.jsx

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "4220873385b67405"; }
+/******/ 		__webpack_require__.h = function() { return "89bc1d6d309e1f29"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/index.jsx
+++ pages/index.jsx
@@ -9,7 +9,7 @@ export default function Home() {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>Hi, I'm shoshana! I'm a software engineer based in NYC.</p>
+        <p>Hi, I'm Shoshana! I'm a software engineer based in NYC.</p>
         <p>
           (This is a sample website - you’ll be building a site like this on{" "}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)

0fd75cca4a876978fc72011af788bb55ede348c7
shoshana.levitt@gmail.com <shoshana.levitt@gmail.com>
2023-11-05 20:00:20 -0500

    add Link from first-page to homepage

--- .next/static/webpack/webpack.54036173d73213f3.hot-update.js
+++ .next/static/webpack/webpack.54036173d73213f3.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "8799df6d7db8fadb"; }
+/******/ 	__webpack_require__.h = function() { return "fc0ecae96dc3131a"; }
 /******/ }();
 /******/ 
 /******/ }

3d7bcc0b963ce8a15440fc3ce861fbeb50545ddb
shoshana.levitt@gmail.com <shoshana.levitt@gmail.com>
2023-11-06 16:30:18 -0500

    add components folder, layout.js, layout.module.css

--- .next/static/webpack/webpack.32296fe2856f8fd2.hot-update.js
+++ .next/static/webpack/webpack.32296fe2856f8fd2.hot-update.js
@@ -11,7 +11,12 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "1056c09383dd7c74"; }
+/******/ 	__webpack_require__.h = function() { return "0fd1ef26535648cf"; }
+/******/ }();
+/******/ 
+/******/ /* webpack/runtime/nonce */
+/******/ !function() {
+/******/ 	__webpack_require__.nc = undefined;
 /******/ }();
 /******/ 
 /******/ }

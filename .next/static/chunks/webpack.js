32220e29e49ae56da0d55c83d3183ce4c4292eb8
shoshana.levitt@gmail.com <shoshana.levitt@gmail.com>
2023-11-07 11:01:38 -0500

    add posts dir, add md files, add lib dir, add posts.js file

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "89bc1d6d309e1f29"; }
+/******/ 		__webpack_require__.h = function() { return "680fcb0ba540e817"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */

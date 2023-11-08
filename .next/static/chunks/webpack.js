9a0a00398219bacce701bd3e5ebdc4d192ae7bd6
shoshana.levitt@gmail.com <shoshana.levitt@gmail.com>
2023-11-08 14:37:10 -0500

    add allPostsData to index.jsx, add section to Home component

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "680fcb0ba540e817"; }
+/******/ 		__webpack_require__.h = function() { return "d9c9bcfd1e79ac7e"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
